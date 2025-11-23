e96ceb4fa31b17dd9b09e1b8e5be08e7f770107f
dmydna <danimaydana9@gmail.com>
2025-11-23 11:52:04 -0300

    completa integrador ej3

--- src/7 - Ejercicios integradores/ej3/ej3c/ej3c.c
+++ src/7 - Ejercicios integradores/ej3/ej3c/ej3c.c
@@ -1,5 +1,44 @@
 #include "../ejs.h"
 
 estadisticas_t* calcular_estadisticas(caso_t* arreglo_casos, int largo, uint32_t usuario_id){
+
+      estadisticas_t* estadisticas = calloc(1, sizeof(estadisticas_t));
+
+      for(int i=0; i<largo;i++){
+        if(usuario_id != 0 && arreglo_casos[i].usuario->id == usuario_id){
+            cantidad_por_casos(estadisticas, &arreglo_casos[i]);
+        }
+        if(usuario_id == 0){
+            cantidad_por_casos(estadisticas, &arreglo_casos[i]);
+        }
+      }
+
+    return estadisticas;
+}
+
+void cantidad_por_casos(estadisticas_t* estadisticas, caso_t* caso){
+    
+    if(strcmp(caso->categoria, "CLT") == 0){
+        estadisticas->cantidad_CLT ++;
+    };
+    if(strcmp(caso->categoria, "RBO") == 0){
+        estadisticas->cantidad_RBO ++;
+    };
+    if(strcmp(caso->categoria, "KSC") == 0){
+        estadisticas->cantidad_KSC ++;
+    };
+    if(strcmp(caso->categoria, "KDT") == 0){
+        estadisticas->cantidad_KDT ++;
+    }
+
+    if(caso->estado == 0){
+        estadisticas->cantidad_estado_0++;
+    }
+    if(caso->estado == 1){
+        estadisticas->cantidad_estado_1++;
+    }
+    if(caso->estado == 2){
+        estadisticas->cantidad_estado_2++;
+    }
 }
 
